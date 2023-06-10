4c92e7169e1d9e6d8de0179497b4926d0ee6bf2e
Alexander Vainshtein <alexanderv@wix.com>
2023-06-11 00:30:34 +0300

    Bump version 1.0.4

--- src/excersises/reset/index.ts
+++ src/excersises/reset/index.ts
@@ -8,6 +8,7 @@ function factory(context: ExcerciseContext, soft: boolean): Excercise {
     ? `- cmd[git reset --sort HEAD~N] to set the HEAD pointer to sv[N] commits back (N could be 0) bold[without changing the file system state]`
     : `- cmd[git reset --hard HEAD~N] to set the HEAD pointer to sv[N] commits back (N could be 0) and bold[change the file system state as it was on that commit]`;
 
+  const opt = soft ? "soft" : "hard";
   return new Excercise({
     context,
     topic:
@@ -15,11 +16,9 @@ function factory(context: ExcerciseContext, soft: boolean): Excercise {
       ` - the current position of the HEAD pointer (normally points to the last commit in the repo)\n` +
       ` - the current state of the file system (thst is, the actual content of the files)\n\n` +
       `The cmd[git reset] command changs one or both of these parts - depending on the psrameters.  \n` +
-      `We will take look at one of the most useful: opt[${
-        soft ? "--soft" : "--hard"
-      }].`,
+      `We will take look at one of the most useful: opt[--${opt}].`,
     description: `Repo has few commits. You will play with resetting the state and inspecting it afterwards. Use\n${text}\n`,
-    tldr: "reset the state",
+    tldr: `reset the state - ${opt}`,
     tasks: [soft ? new ResetSoftTask(context) : new ResetHardTask(context)],
   });
 }
