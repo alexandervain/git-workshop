9a2b1fcd8fcf7dbfafbe7657e72a6386ea9b9bd6
Alexander Vainshtein <alexanderv@wix.com>
2023-06-11 21:28:35 +0300

    Fix typo

--- src/excersises/squash/index.ts
+++ src/excersises/squash/index.ts
@@ -11,7 +11,7 @@ export function factory(context: ExcerciseContext): Excercise {
       `The content of a commit marked with that option will be added to the previous commit and also the commit message will be appened to the message of the previous commit.\n` +
       `${REBASING_WARNING}`,
     description:
-      `Repo has 5 commits. You will need to unite (squash) 3 last commits` +
+      `Repo has a few commits. You will need to unite (squash) 3 last commits` +
       `into a single commit preserving all the commit messages (so there will be 2 commits in total).\n` +
       `Use the stress[ineractive rebase] and the opt[squash] option`,
     tldr: "squash commits",
